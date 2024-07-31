ac2de25108ec3421e5c97fe609df8eae2dfba677
Bogdan Isaev <redzor@live.com>
2024-07-31 14:33:31 +0300

    security check add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,13 @@ import {AboutComponent} from "../about/about.component";
 import {NaviComponent} from "../navi/navi.component";
 import {DeployComponent} from "../deploy/deploy.component";
 import {SkillsComponent} from "../skills/skills.component";
+import {DeviceDetectorService} from "ngx-device-detector";
+import {MobileComponent} from "../mobile/mobile.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SectionsComponent, NaviComponent],
+  imports: [RouterOutlet, SectionsComponent, NaviComponent, MobileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -19,4 +21,6 @@ export class AppComponent {
   components = [HomeComponent, OffersComponent, SkillsComponent, DeployComponent, AboutComponent];
   routes = ['home', 'offers', 'skills', 'deploy', 'about'];
   protected readonly NaviComponent = NaviComponent;
+  constructor(public d: DeviceDetectorService) {
+  }
 }
